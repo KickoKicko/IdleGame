c627fc92626e2a55d1347394eba21d1c869f33da
KickoKicko <kickokicko110@gmail.com>
2023-08-03 21:24:13 +0200

    More js

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 let money = 0;
+let test2 = 0;
 const mainButton = document.getElementById("mainButton");
 mainButton.addEventListener("click",add_money);
 
 function add_money(){
-console.log("fff");
 money = money+1;
-console.log(money);
 update_money();
+test();
 }
 
 
 function update_money(){
     var e = document.getElementById("moneyDisplay");
-    e.innerHTML = money;
+    e.innerHTML = "Money: "+money;
+}
+
+function test(){
+    var e = document.getElementById("loader");
+    test2 = test2 + 5;
+    e.style.width = test2+"%";
 }
