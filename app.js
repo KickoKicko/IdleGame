6789c6d585c0d3524d2f06eb4faa9ed6a9b7a668
KickoKicko <kickokicko110@gmail.com>
2023-08-03 20:51:44 +0200

    Learning js basics

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-var money = 0;
-
-//document.getElementById("mainButton").onclick = add_money()
-//document.getElementById("mainButton").addEventListener("click", function add_money)
-
+let money = 0;
+const mainButton = document.getElementById("mainButton");
+mainButton.addEventListener("click",add_money);
 
 function add_money(){
 console.log("fff");
-money = money+1
+money = money+1;
+console.log(money);
+update_money();
 }
+
 
 function update_money(){
-    var e = document.getElementById("moneyDisplay")
-    e.innerHTML = money
+    var e = document.getElementById("moneyDisplay");
+    e.innerHTML = money;
 }
-
-
-
-//$(document).ready(function(){
-
-//})
