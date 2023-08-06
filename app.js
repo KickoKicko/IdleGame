7fed006260e0a20db6af34c82b18723941bd2587
KickoKicko <kickokicko110@gmail.com>
2023-08-06 14:16:28 +0200

    Monster Cards and monster classes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let money = 0;
 let test2 = 0;
+let myMonsterArray = [];
+let allMonsterTypesByID = ["Arguu", "Ladlu", "Tryon"];
+let singleMonsterDisplay = null;
 const mainButton = document.getElementById("mainButton");
 mainButton.addEventListener("click",add_money);
 
@@ -8,9 +11,11 @@ mainButton.addEventListener("click",add_money);
 
 
 function add_money(){
-money = money+1;
+money++;
 update_money();
 test();
+myMonsterArray.push(new Arguu("Common"));
+updateMonsterCards();
 }
 
 
@@ -23,4 +28,29 @@ function test(){
     var e = document.getElementById("loader");
     test2 = test2 + 5;
     e.style.width = test2+"%";
+}
+
+function updateMonsterCards(){
+    let arrayLength = myMonsterArray.length;
+    let holder = document.getElementById("allMonsterHolder");
+    let monsterCountingArray = [0,0,0];
+    let idleMonsterCountingArray = [0,0,0];
+
+    for(let i = 0;i<arrayLength;i++){//Counts how many of every type of monster there are
+        monsterCountingArray[myMonsterArray[i].monsterId-1]++;
+    }
+
+    for(let i = 0;i<arrayLength;i++){//Counts how many of every type of monster there are that currently idle
+        for(let j = 0;j<monsterCountingArray[i];j++){
+            idleMonsterCountingArray[i]++;
+        }
+    }
+
+    
+    
+    for(let i = 0;i<monsterCountingArray.length;i++){// Adds the name and amount to the monster cards
+        holder.children[i].children[0].innerHTML = allMonsterTypesByID[i];
+        holder.children[i].children[1].innerHTML = "Amount: "+monsterCountingArray[i]+"X";
+        holder.children[i].children[2].innerHTML = "Idle: "+idleMonsterCountingArray[i]+"X";
+    }
 }
--- monsterStats.js
+++ monsterStats.js
@@ -7,26 +7,52 @@ class Monster{
         this.reproducibility = "Empty";
         this.blood = "Empty";
         this.rarity = "Empty";
+        this.monsterId = "Empty";
+        this.status = "Idle";
+    }
+
+    get getName(){
+        return this.name;
+    }
+    get getMonsterId(){
+        return this.monsterId;
+    }
+    get getStatus(){
+        return this.status;
+    }
+    set setStatus(s){
+        this.status = s;
     }
 
     show(){
-        console.log(this.name + this.type + this.resource + this.rarity);
+        console.log(this.name + this.type + this.resource + this.rarity+this.income);
     }
 }
 
 class Arguu extends Monster{
-    constructor(rarity, income, reproducibility, blood){
-        super(rarity, income, reproducibility, blood);
+    constructor(rarity){
+        super(rarity);
         this.name = "Arguu";
         this.type = "Fire";
-        this.income = income;
         this.resource = "Gunpowder";
-        this.reproducibility = reproducibility;
-        this.blood = blood;
+        this.monsterId = 1;
         this.rarity = rarity;
+
+        if(rarity == "Common"){
+            this.income = 3;
+            this.reproducibility = 1.12;
+            this.blood = 43;
+        }
+        else if(rarity == "Uncommon"){
+            this.income = 4;
+            this.reproducibility = 1.18;
+            this.blood = 54;
+        }
+        
+        
     }
 }
-let arguuCommon = new Arguu("Common" ,3 ,1.12 ,43);
-let arguuUncommon = new Arguu("Uncommon" ,4 ,1.18 ,54);
+let arguuCommon = new Arguu("Common");
+let arguuUncommon = new Arguu("Uncommon");
 arguuCommon.show();
 arguuUncommon.show();
